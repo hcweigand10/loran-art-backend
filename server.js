71f21505d5d43926f16b3e27a8f0a7ede1497ec2
hcweigand10 <henryweigand10@gmail.com>
2023-03-01 13:44:20 -0800

    options status

--- server.js
+++ server.js
@@ -34,6 +34,9 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Methods', 'GET, OPTIONS, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.header('Access-Control-Allow-Credentials', true);
+  if (req.method === "OPTIONS") {
+    res.status(200)
+  }
   return next();
 });
 
