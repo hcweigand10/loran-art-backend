ddd812957276ef1cf31768bbf8c0987336f924f9
hcweigand10 <henryweigand10@gmail.com>
2023-04-12 17:19:01 -0700

    route index include tags

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 const artRoutes = require("./artRoutes")
 const categoryRoutes = require("./categoryRoutes")
+const tagRoutes = require("./tagRoutes")
 const userRoutes = require("./userRoutes")
 
 router.use("/art", artRoutes)
+router.use("/tags", tagRoutes)
 router.use("/categories", categoryRoutes)
 router.use("/users", userRoutes)
 
