173d96d5e954e4476b8a2e15c679a9634f004f12
hcweigand10 <henryweigand10@gmail.com>
2023-08-16 14:35:21 -0700

    server

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,27 @@
-const Sequelize = require('sequelize');
-require('dotenv').config();
+const Sequelize = require("sequelize");
+require("dotenv").config();
 
 let sequelize;
 
-// if (process.env.JAWSDB_URL) {
-//   sequelize = new Sequelize(process.env.JAWSDB_URL);
-// } else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: process.env.DB_HOST,
-      dialect: 'mysql',
-      port: 3306
-    }
-  );
-// }
+sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    port: 3306,
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+  }
+);
+
 
 module.exports = sequelize;
+
+
+
+// const uri2 =
+//   "mysql://dkiihr6wvz4gflhp:p7w3o3evbmt8xjxv@ohunm00fjsjs1uzy.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/um25yozm7dwasvhq";
+// const uri = `mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:3306/${process.env.DB_NAME}`;
+
+// console.log(uri);
+// sequelize = new Sequelize(uri2);
