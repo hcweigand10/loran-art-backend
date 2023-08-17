d55fa1593094455acbd5b8b02344a3c965a911d2
hcweigand10 <henryweigand10@gmail.com>
2023-08-17 09:58:09 -0700

    server

--- config/connection.js
+++ config/connection.js
@@ -18,9 +18,3 @@ sequelize = new Sequelize(
 module.exports = sequelize;
 
 
-// const uri2 =
-//   "mysql://dkiihr6wvz4gflhp:p7w3o3evbmt8xjxv@ohunm00fjsjs1uzy.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/um25yozm7dwasvhq";
-// const uri = `mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:3306/${process.env.DB_NAME}`;
-
-// console.log(uri);
-// sequelize = new Sequelize(uri2);
