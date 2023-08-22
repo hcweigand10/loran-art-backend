b9fd0175ab9802aa195f33676c3b408270950d0b
hcweigand10 <henryweigand10@gmail.com>
2023-08-22 10:53:04 -0700

    jaws

--- config/connection.js
+++ config/connection.js
@@ -3,8 +3,8 @@ require("dotenv").config();
 
 let sequelize;
 // if (process.env.JAWSDB_URL) {
-if (false) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
+// if (false) {
+  // sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   sequelize = new Sequelize(
     process.env.DB_NAME,
