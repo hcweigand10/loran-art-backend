3abce228394d1af0b159677fa8a89fc41585d2d2
hcweigand10 <henryweigand10@gmail.com>
2023-09-07 12:07:10 -0700

    cors

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.static('public'));
 
 
 app.use((req, res, next) => {
-  const allowedOrigins = ['http://localhost:3000', ]; // add deployed URL here
+  const allowedOrigins = ['http://localhost:3000', "https://loranscruggs.netlify.app"]; // add deployed URL here
   const origin = req.headers.origin;
   if (allowedOrigins.includes(origin)) {
        res.setHeader('Access-Control-Allow-Origin', origin);
